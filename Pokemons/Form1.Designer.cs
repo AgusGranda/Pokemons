d1930fc0543b24458f22b8322f211fe4bdb5ce19
Agustin Granda <118733656+AgusGranda@users.noreply.github.com>
2023-11-14 13:20:51 -0300

    first commit

---
+++ Pokemons/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Pokemons
+{
+    partial class frmPokemons
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvPokemons = new System.Windows.Forms.DataGridView();
+            this.pbxPokemons = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPokemons)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPokemons)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvPokemons
+            // 
+            this.dgvPokemons.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPokemons.Location = new System.Drawing.Point(12, 45);
+            this.dgvPokemons.Name = "dgvPokemons";
+            this.dgvPokemons.Size = new System.Drawing.Size(513, 205);
+            this.dgvPokemons.TabIndex = 0;
+            // 
+            // pbxPokemons
+            // 
+            this.pbxPokemons.Location = new System.Drawing.Point(545, 45);
+            this.pbxPokemons.Name = "pbxPokemons";
+            this.pbxPokemons.Size = new System.Drawing.Size(228, 205);
+            this.pbxPokemons.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxPokemons.TabIndex = 1;
+            this.pbxPokemons.TabStop = false;
+            // 
+            // frmPokemons
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 363);
+            this.Controls.Add(this.pbxPokemons);
+            this.Controls.Add(this.dgvPokemons);
+            this.Name = "frmPokemons";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pokemons";
+            this.Load += new System.EventHandler(this.frmPokemons_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPokemons)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPokemons)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvPokemons;
+        private System.Windows.Forms.PictureBox pbxPokemons;
+    }
+}
+
---
+++ Pokemons/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pokemons
+{
+    public partial class frmPokemons : Form
+    {
+
+        List<Pokemon> pokemons;
+
+        public frmPokemons()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPokemons_Load(object sender, EventArgs e)
+        {
+
+            PokemonNegocio pokemonNegocio = new PokemonNegocio();
+
+            pokemons = pokemonNegocio.listar();
+
+            dgvPokemons.DataSource = pokemons;
+
+            pbxPokemons.Load(pokemons[0].UrlImagen);
+
+
+        }
+    }
+}
